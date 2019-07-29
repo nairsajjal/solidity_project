d6684a9261876da493abadccde94556cf2a3d34d
sajjal <nairsajjal@gmail.com>
2019-07-29 20:31:58 +0530

    added the first candidate

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.8;
+
+contract Election{
+    //store candidate
+    //read candidate
+     string public candidate;
+    //Constructor
+    constructor() public{
+        candidate = "Candidate 1";
+    }
+}
