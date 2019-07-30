35b689dbbc5b9eee622291cf5cb1417458c88c77
sajjal <nairsajjal@gmail.com>
2019-07-30 11:25:27 +0530

    added another test

--- test/election.js
+++ test/election.js
@@ -1,5 +1,7 @@
 var Election  = artifacts.require("./Election.sol");
 contract("Election", function(accounts){
+    var electionInstance;
+
     it("initialize with two candidates", function(){
         return Election.deployed().then(function(instance){
             return instance.candidatesCount();
@@ -7,5 +9,23 @@ contract("Election", function(accounts){
             assert.equal(count, 2);
         });
     });
+
+    it("it initializes the candidates with the correct values", function() {
+        return Election.deployed().then(function(instance) {
+          electionInstance = instance;
+          return electionInstance.candidates(1);
+        }).then(function(candidate) {
+          assert.equal(candidate[0], 1, "contains the correct id");
+          assert.equal(candidate[1], "Candidate 1", "contains the correct name");
+          assert.equal(candidate[2], 0, "contains the correct votes count");
+          return electionInstance.candidates(2);
+        }).then(function(candidate) {
+          assert.equal(candidate[0], 2, "contains the correct id");
+          assert.equal(candidate[1], "Candidate 2", "contains the correct name");
+          assert.equal(candidate[2], 0, "contains the correct votes count");
+        });
+      });
+      
+
 
 });
